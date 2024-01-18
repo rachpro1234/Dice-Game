6aec2c4a7967f3b49d2f17e56d1e37a42166edad
Rachid El Barqouqy <108005388+rachpro1234@users.noreply.github.com>
2024-01-18 20:06:08 +0100

    initial

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from './App.jsx'
 import './index.css'
 import { I18nextProvider } from "react-i18next";
 import i18next from "i18next";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
 
 i18next.init({
@@ -14,11 +14,11 @@ i18next.init({
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-      <HashRouter basename='/Dice-Game'>
+      <BrowserRouter basename='/Dice-Game'>
           <I18nextProvider i18n={i18next}>
               <App />
           </I18nextProvider>
-      </HashRouter>
+      </BrowserRouter>
   </React.StrictMode>,
 
 )
