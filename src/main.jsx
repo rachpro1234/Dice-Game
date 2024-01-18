d7d4ce719fae1567ae74de1e6bdb0f4adea22595
Rachid El Barqouqy <108005388+rachpro1234@users.noreply.github.com>
2024-01-18 15:52:38 +0100

    route changes

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import App from './App.jsx'
 import './index.css'
 import { I18nextProvider } from "react-i18next";
 import i18next from "i18next";
+import { BrowserRouter } from "react-router-dom";
 
 
 i18next.init({
@@ -13,9 +14,11 @@ i18next.init({
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <I18nextProvider i18n={i18next}>
-        <App />
-    </I18nextProvider>
+      <BrowserRouter basename='/Dice-Game'>
+          <I18nextProvider i18n={i18next}>
+              <App />
+          </I18nextProvider>
+      </BrowserRouter>
   </React.StrictMode>,
 
 )
